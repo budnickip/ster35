dbe330e90870613682c705b1c6be432b7ebcbc8c
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-03-15 13:46:53 +0100

    add map with grey filters

--- src/index.js
+++ src/index.js
@@ -9,3 +9,17 @@ import contactNav from './scripts/contact-nav.js';
 menu()
 contact()
 contactNav()
+
+var mymap = L.map('mapid').setView([53.427371, 14.508916], 16);
+
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiYnVkeW45NSIsImEiOiJja2xzNWpzb3gwdm05MnZsbHJiYmFpYXp4In0.ll8NGCRsayEEhLowbI8f8A'
+}).addTo(mymap);
+
+ L.marker([53.427371, 14.508916]).addTo(mymap);
